7a93242ca70561d936ceb95a5dd2b3abfce27481
aniket30i <iamaniket.saha.09@protonmail.com>
2024-04-02 23:11:55 +0530

    Pratcing props and components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App(params) {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="ironman.jpg" alt="Aniket" />;
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="😋" color="skyblue" />
+      <Skill skill="Html+CSS" emoji="😎" color="orangered" />
+      <Skill skill="Javascript" emoji="😍" color="yellow" />
+      <Skill skill="Python" emoji="👌" color="green" />
+    </div>
+  );
+}
+function Intro(params) {
+  return (
+    <div>
+      <h1>Aniket Saha</h1>
+      <p>Budding SoftwareDeveloper Dreams to make an impact on the world</p>
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
